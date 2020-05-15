800421956b8e79dd3c29436f2e7ade06afe4b8c1
Ankit <ankit.mohane@ondotsystems.com>
2020-05-15 22:56:21 +0530

    Update version changes

--- src/main/java/com/moviecatlog/moviecatlogconfigurationserver/MoviecatlogConfigurationServerApplication.java
+++ src/main/java/com/moviecatlog/moviecatlogconfigurationserver/MoviecatlogConfigurationServerApplication.java
@@ -1,13 +1,24 @@
 package com.moviecatlog.moviecatlogconfigurationserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
+@RestController
 public class MoviecatlogConfigurationServerApplication {
-
+	
+	@Value("${app.version}")
+	private String appVersion;
+	
+	@GetMapping("version")
+	public String getAppVersion() {
+		return appVersion;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MoviecatlogConfigurationServerApplication.class, args);
 	}
